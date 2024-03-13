ec8722aeba52ad96f9470456f5c3af8df16b566b
molenaru <121340870+molenaru@users.noreply.github.com>
2024-03-14 00:18:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ window.onload = function() {
 
   const menu_btn = document.querySelector('.hamburger');
   const mobile_menu = document.querySelector('.mobile-nav');
+  const navigasi = document.querySelector('.navigasi');
+
 
   menu_btn.addEventListener('click', function() {
       menu_btn.classList.toggle('is-active');
       mobile_menu.classList.toggle('is-active');
+      navigasi.classList.toggle('is-active');
   });
 }
